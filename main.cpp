4a51d30ca30bcb0981394908396d61095e0fcf6c
s4ncheez <89919448+s4ncheez@users.noreply.github.com>
2022-03-12 11:25:58 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,15 +6,8 @@ using namespace Class;
 
 int main() {
     //File_stream fin{"testlab.txt"};
-    File_stream fin{};
-    bool cond = fin.open();
-    cout << cond << endl;
-    string line = fin.get_next_word();
-    cout << line << endl;
-    string line2 = fin.get_next_line();
-    cout << line2 << endl;
-    bool check = fin.check_is_end();
-    cout << check << endl;
+    File_stream fin;
+    fin.set_file_name("default2.txt");
     string namefile = fin.getname();
     cout << namefile << endl;
     return 0;
